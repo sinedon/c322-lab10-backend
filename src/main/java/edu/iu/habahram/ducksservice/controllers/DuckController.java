bbbe41059f73c01d786643ec84b2095771fc926e
sinedon <111921789+sinedon@users.noreply.github.com>
2024-04-18 21:25:35 -0400

    Updated Lab10

--- src/main/java/edu/iu/habahram/ducksservice/controllers/DuckController.java
+++ src/main/java/edu/iu/habahram/ducksservice/controllers/DuckController.java
@@ -1,7 +1,7 @@
 package edu.iu.habahram.ducksservice.controllers;
 
 import edu.iu.habahram.ducksservice.model.DuckData;
-import edu.iu.habahram.ducksservice.model.Duck;
+import edu.iu.habahram.ducksservice.repository.DucksFileRepository;
 import edu.iu.habahram.ducksservice.repository.DucksRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,90 +18,62 @@ import java.util.List;
 public class DuckController {
 
     private DucksRepository ducksRepository;
+    private DucksFileRepository ducksFileRepository;
 
     public DuckController(DucksRepository ducksRepository) {
         this.ducksRepository = ducksRepository;
     }
 
-
-   @PostMapping
-    public int add(@RequestBody DuckData duck) {
-       try {
-           return ducksRepository.add(duck);
-       } catch (IOException e) {
-           throw new RuntimeException(e);
-       }
+    @PostMapping
+    public DuckData add(@RequestBody DuckData duck) {
+       return ducksRepository.save(duck);
    }
 
     @GetMapping
     public List<DuckData> findAll() {
-        try {
-            return ducksRepository.findAll();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        return ducksRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<DuckData> find(@PathVariable int id) {
-        try {
-            DuckData duck = ducksRepository.find(id);
-            if(duck != null) {
-                return ResponseEntity
-                        .status(HttpStatus.FOUND)
-                        .body(duck);
-            } else {
-                return ResponseEntity
-                        .status(HttpStatus.NOT_FOUND)
-                        .body(null);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        DuckData duck = ducksRepository.findById(id);
+        if(duck != null) {
+            return ResponseEntity
+                    .status(HttpStatus.FOUND)
+                    .body(duck);
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(null);
         }
     }
 
     @PostMapping("/{id}/image")
     public boolean updateImage(@PathVariable int id,
-                               @RequestParam MultipartFile file) {
-        try {
-            return ducksRepository.updateImage(id, file);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+                               @RequestParam MultipartFile file) throws IOException {
+        return ducksFileRepository.updateImage(id, file);
     }
 
     @PostMapping("/{id}/audio")
     public boolean updateAudio(@PathVariable int id,
-                               @RequestParam MultipartFile file) {
-        try {
-            return ducksRepository.updateAudio(id, file);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+                               @RequestParam MultipartFile file) throws IOException {
+        return ducksFileRepository.updateAudio(id, file);
     }
 
     @GetMapping("/{id}/image")
-    public ResponseEntity<?> getImage(@PathVariable int id) {
-        try {
-            byte[] image = ducksRepository.getImage(id);
-            return ResponseEntity.status(HttpStatus.FOUND)
-                    .contentType(MediaType.IMAGE_PNG)
-                    .body(image);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public ResponseEntity<?> getImage(@PathVariable int id) throws IOException {
+        byte[] image = ducksFileRepository.getImage(id);
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .contentType(MediaType.IMAGE_PNG)
+                .body(image);
     }
 
     @GetMapping("/{id}/audio")
-    public ResponseEntity<?> getAudio(@PathVariable int id) {
-        try {
-            byte[] image = ducksRepository.getAudio(id);
-            return ResponseEntity.status(HttpStatus.FOUND)
-                    .contentType(MediaType.valueOf("audio/mp3"))
-                    .body(image);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public ResponseEntity<?> getAudio(@PathVariable int id) throws IOException {
+        byte[] image = ducksFileRepository.getAudio(id);
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .contentType(MediaType.valueOf("audio/mp3"))
+                .body(image);
     }
 
 }
--- src/main/java/edu/iu/habahram/ducksservice/model/DuckData.java
+++ src/main/java/edu/iu/habahram/ducksservice/model/DuckData.java
@@ -1,6 +1,26 @@
 package edu.iu.habahram.ducksservice.model;
 
-public record DuckData(int id, String name, String type) {
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(schema = "ducks")
+public final class DuckData {
+
+    @Id
+    private int id;
+    private String name;
+    private String type;
+
+    public DuckData() {
+
+    }
+    public DuckData(int id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
 
     public String toLine() {
         return String.format("%1$s,%2$s,%3$s", id(), name(), type());
@@ -15,4 +35,38 @@ public record DuckData(int id, String name, String type) {
         return new DuckData(Integer.parseInt(tokens[0]), tokens[1], tokens[2]);
     }
 
+    public int id() {
+        return id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String type() {
+        return type;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (DuckData) obj;
+        return this.id == that.id &&
+                Objects.equals(this.name, that.name) &&
+                Objects.equals(this.type, that.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, type);
+    }
+
+    @Override
+    public String toString() {
+        return "DuckData[" +
+                "id=" + id + ", " +
+                "name=" + name + ", " +
+                "type=" + type + ']';
+    }
 }
